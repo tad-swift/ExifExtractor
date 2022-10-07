2a5ef6ea7a9390ad478caff82ebfc3daf9e6cb6c
Tadreik Campbell <tadreik@gmail.com>
2022-10-07 13:59:15 -0400

    Initial Commit

---
+++ ExifExtractor/ExifExtractor.h
@@ -0,0 +1,18 @@
+//
+//  ExifExtractor.h
+//  ExifExtractor
+//
+//  Created by Tadreik Campbell on 10/7/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ExifExtractor.
+FOUNDATION_EXPORT double ExifExtractorVersionNumber;
+
+//! Project version string for ExifExtractor.
+FOUNDATION_EXPORT const unsigned char ExifExtractorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExifExtractor/PublicHeader.h>
+
+
